07861e4b4b28a90e67872eda5ecb252bc94f358b
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-03-22 23:41:07 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
-// https://vitejs.dev/config/
+import json from '@rollup/plugin-json';
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+  },
+  esbuild: {
+    json: {
+      stringify: true,
+    },
   },
 });
